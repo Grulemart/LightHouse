1a5fee33a37323c198d7bc4388a624bb50f50b96
Tastac <Keyganap@gmail.com>
2019-04-19 23:47:31 -0700

    Prototype Setup
    Setup a basic prototype scene using shapes.
    
    uses Android build type so make sure you have it installed before opening.
    
    updated LightMoveController to not spasm when looking at Lighthouse or Land and no longer moves light when looking at nothing(i.e. Sky)

--- Assets/Scripts/LightMoveController.cs
+++ Assets/Scripts/LightMoveController.cs
@@ -6,25 +6,31 @@ public class LightMoveController : MonoBehaviour
 {
     [HideInInspector]
     public RaycastHit hit;
+    [HideInInspector]
+    public int layerMask = (1<<9);
     public GameObject lighttracker;
 
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.touchCount > 0)
         {
             //Turns touch input into Vector2
             Vector2 touchPos = Input.touches[0].position;
             //cast's a ray from camera in the direction of touchPos and casts a second ray to get hit position
             Ray r = Camera.main.ScreenPointToRay(new Vector3(touchPos.x, touchPos.y, 0));
-            Physics.Raycast(r, out hit);
+            Physics.Raycast(r, out hit,Mathf.Infinity, ~layerMask);
             //Draws ray in editor for debugging (Remove Before build)
             Debug.DrawRay(r.origin, r.direction * 10000, Color.yellow);
-            //Delay When moving light around (WIP)
-            lighttracker.transform.position = Vector3.MoveTowards(lighttracker.transform.position, hit.point, 1);
-            //rotate Light to look at lighttracker
-            transform.LookAt(lighttracker.transform.position);
+            if (hit.transform != null)
+            {
+                //Delay When moving light around (WIP)
+                lighttracker.transform.position = Vector3.MoveTowards(lighttracker.transform.position, hit.point, 1);
+                //rotate Light to look at lighttracker
+                transform.LookAt(lighttracker.transform.position);
+            }
         }
     }
 }
