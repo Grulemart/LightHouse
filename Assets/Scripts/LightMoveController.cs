1200526fc62097ff07a679916b4bd7ca3d8295c9
Tastac <Keyganap@gmail.com>
2019-04-21 08:57:22 -0700

    Update LightMoveController.cs

--- Assets/Scripts/LightMoveController.cs
+++ Assets/Scripts/LightMoveController.cs
@@ -14,24 +14,37 @@ public class LightMoveController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //PC Control
         if (Input.GetKey(KeyCode.Mouse0))
         {
             //Turns touch input into Vector2
             Vector2 touchPos = Input.mousePosition;
-            //cast's a ray from camera in the direction of touchPos and casts a second ray to get hit position
-            Ray r = Camera.main.ScreenPointToRay(new Vector3(touchPos.x, touchPos.y, 0));
-            Physics.Raycast(r, out hit,Mathf.Infinity, ~layerMask);
-            //Draws ray in editor for debugging (Remove Before build)
-            Debug.DrawRay(r.origin, r.direction * 10000, Color.yellow);
-            Debug.Log(hit.transform);
-            if (hit.transform != null)
-            {
-                //Delay When moving light around (WIP)
-                lighttracker.transform.position = Vector3.MoveTowards(lighttracker.transform.position, hit.point, 1);
-                //rotate Light to look at lighttracker
-                transform.LookAt(lighttracker.transform.position);
-            }
+            RayCastLight(touchPos.x, touchPos.y);
+        }
+        //Mobile Control
+        if (Input.touchCount > 0)
+        {
+            //Turns touch input into Vector2
+            Vector2 touchPos = Input.touches[0].position;
+            RayCastLight(touchPos.x, touchPos.y);
+        }
+
+    }
+
+    void RayCastLight(float PosX, float PosY)
+    {
+        //cast's a ray from camera in the direction of touchPos and casts a second ray to get hit position
+        Ray r = Camera.main.ScreenPointToRay(new Vector3(PosX, PosY, 0));
+        Physics.Raycast(r, out hit, Mathf.Infinity, ~layerMask);
+        //Draws ray in editor for debugging (Remove Before build)
+        Debug.DrawRay(r.origin, r.direction * 10000, Color.yellow);
+        if (hit.transform != null)
+        {
+            //Delay When moving light around (WIP)
+            lighttracker.transform.position = Vector3.MoveTowards(lighttracker.transform.position, hit.point, 1);
+            //rotate Light to look at lighttracker
+            transform.LookAt(lighttracker.transform.position);
         }
     }
+
 }
